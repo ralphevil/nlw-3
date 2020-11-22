7b4b05400ed8848592740a8a1ae75ca421f7a4a1
Raphael Souza <ralphevils5@gmail.com>
2020-11-22 01:28:55 -0300

    Criação Web

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+//JSX - Javascript XML //TSX - Typescript XML
+import React from 'react';
+
+import 'leaflet/dist/leaflet.css'
+import './styles/global.css'
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
